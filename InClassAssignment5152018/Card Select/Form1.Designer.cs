1392d12c2a5f662ed9bed2fa0d21bccb13b98932
ArtAmmirati <34355879+ArtAmmirati@users.noreply.github.com>
2018-05-15 20:48:23 -0400

    UpLoad

---
+++ InClassAssignment5152018/Card Select/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace Card_Select
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(0, 59);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(133, 238);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(-3, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(121, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Pick A Card ... Any Card";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(148, 274);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Show All";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "2 of Clubs");
+            this.imageList1.Images.SetKeyName(1, "2 of Diamonds");
+            this.imageList1.Images.SetKeyName(2, "2 of Hearts");
+            this.imageList1.Images.SetKeyName(3, "2 of Spades");
+            this.imageList1.Images.SetKeyName(4, "3 of Clubs");
+            this.imageList1.Images.SetKeyName(5, "3 of Diamonds");
+            this.imageList1.Images.SetKeyName(6, "3 of Hearts");
+            this.imageList1.Images.SetKeyName(7, "3 of Spades");
+            this.imageList1.Images.SetKeyName(8, "4 of Clubs");
+            this.imageList1.Images.SetKeyName(9, "4 of Diamonds ");
+            this.imageList1.Images.SetKeyName(10, "4 of Hearts ");
+            this.imageList1.Images.SetKeyName(11, "4 of Spades ");
+            this.imageList1.Images.SetKeyName(12, "5 of Clubs ");
+            this.imageList1.Images.SetKeyName(13, "5 of Diamonds ");
+            this.imageList1.Images.SetKeyName(14, "5 of Hearts ");
+            this.imageList1.Images.SetKeyName(15, "5 of Spades ");
+            this.imageList1.Images.SetKeyName(16, "6 of Clubs ");
+            this.imageList1.Images.SetKeyName(17, "6 of Diamonds ");
+            this.imageList1.Images.SetKeyName(18, "6 of Hearts ");
+            this.imageList1.Images.SetKeyName(19, "6 of Spades");
+            this.imageList1.Images.SetKeyName(20, "7 of Clubs");
+            this.imageList1.Images.SetKeyName(21, "7 of Diamonds");
+            this.imageList1.Images.SetKeyName(22, "7 of Hearts");
+            this.imageList1.Images.SetKeyName(23, "7 of Spades");
+            this.imageList1.Images.SetKeyName(24, "8 of Clubs");
+            this.imageList1.Images.SetKeyName(25, "8 of Diamonds ");
+            this.imageList1.Images.SetKeyName(26, "8 of Hearts ");
+            this.imageList1.Images.SetKeyName(27, "8 of Spades");
+            this.imageList1.Images.SetKeyName(28, "9 of Clubs");
+            this.imageList1.Images.SetKeyName(29, "9 of Diamonds");
+            this.imageList1.Images.SetKeyName(30, "9 of Hearts");
+            this.imageList1.Images.SetKeyName(31, "9 of Spades");
+            this.imageList1.Images.SetKeyName(32, "10 of Clubs");
+            this.imageList1.Images.SetKeyName(33, "10 of Diamonds");
+            this.imageList1.Images.SetKeyName(34, "10 of Hearts");
+            this.imageList1.Images.SetKeyName(35, "10 of Spades");
+            this.imageList1.Images.SetKeyName(36, "Ace of Clubs");
+            this.imageList1.Images.SetKeyName(37, "Ace of Diamonds");
+            this.imageList1.Images.SetKeyName(38, "Ace of Hearts");
+            this.imageList1.Images.SetKeyName(39, "Ace of Spades");
+            this.imageList1.Images.SetKeyName(40, "Backface_Blue");
+            this.imageList1.Images.SetKeyName(41, "Backface_Red");
+            this.imageList1.Images.SetKeyName(42, "Jack of Clubs");
+            this.imageList1.Images.SetKeyName(43, "Jack of Diamonds");
+            this.imageList1.Images.SetKeyName(44, "Jack of Hearts");
+            this.imageList1.Images.SetKeyName(45, "Jack of Spades");
+            this.imageList1.Images.SetKeyName(46, "Joker_Black ");
+            this.imageList1.Images.SetKeyName(47, "Joker_Red ");
+            this.imageList1.Images.SetKeyName(48, "King of Clubs");
+            this.imageList1.Images.SetKeyName(49, "King of Diamonds");
+            this.imageList1.Images.SetKeyName(50, "King of Hearts");
+            this.imageList1.Images.SetKeyName(51, "King of Spades");
+            this.imageList1.Images.SetKeyName(52, "Queen of Clubs");
+            this.imageList1.Images.SetKeyName(53, "Queen of Diamonds");
+            this.imageList1.Images.SetKeyName(54, "Queen of Hearts");
+            this.imageList1.Images.SetKeyName(55, "Queen of Spades");
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(148, 59);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(97, 117);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(675, 364);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Card Selector";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ InClassAssignment5152018/Card Select/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Card_Select
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public int leftPos = 250;
+        public int topPos = 0;
+        public int i = 0;
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            foreach(string card in imageList1.Images.Keys)
+            {
+                imageList1.Images.Keys.ToString();
+                listBox1.Items.Add(card);
+            }
+            
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string CardSelected = listBox1.SelectedItem.ToString();
+            Image Card = imageList1.Images[CardSelected];
+            pictureBox1.BackgroundImage = imageList1.Images[CardSelected];
+            pictureBox1.BackgroundImageLayout = ImageLayout.Stretch;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+          
+            foreach (string card in imageList1.Images.Keys)
+            {
+                PictureBox newCard = new PictureBox();
+                newCard.Size = new Size(70, 140);
+                newCard.Location = new Point(leftPos, topPos);
+                newCard.Image = imageList1.Images[i];
+                this.Controls.Add(newCard);
+                newCard.BringToFront();
+                leftPos = (leftPos + 20);
+
+                if (leftPos > 550)
+                {
+                    topPos = topPos + 75;
+                    leftPos = 250;
+                }
+                i++;
+
+            }
+        }
+    }
+}
